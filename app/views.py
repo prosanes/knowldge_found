cf331d4f5e036f76ab0aa1d4c526340b68c7eff4
Pedro Rosanes <prosanes@gmail.com>
2015-01-05 00:12:34 -0200

    Added index template

--- app/views.py
+++ app/views.py
@@ -1,5 +1,15 @@
 from app import app
+from flask import render_template
 
 @app.route("/")
+def main():
+    return render_template('index.html')
+
+@app.route("/hello")
 def hello():
-    return "Hello World!"
+    return "hello world!"
+
+@app.route('/template/')
+@app.route('/template/<name>')
+def hello2(name=None):
+    return render_template('hello.html', name=name)
